5c7fce60227dc807793380137c6d5488e3fe002b
t-zander <t-zander@t-zanders-MacBook-Pro.local>
2021-04-05 18:07:31 +0300

    initial commit

---
+++ apps/error-regen/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'error-regen',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/error-regen',
+};
